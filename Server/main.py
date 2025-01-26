10d3643034273c6004f967e5a682136af6ca01e4
Achraf Hajji <148371433+hajji-achraf@users.noreply.github.com>
2025-01-26 16:08:44 +0100

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 import pandas as pd
-from scrap import Scrap
+from .scrap import Scrap
 import numpy as np
 
 app = FastAPI()
